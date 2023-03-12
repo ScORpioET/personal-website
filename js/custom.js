6c00d1c18d2dda864f7c202f4d797954dd901fef
ScORpioET <x917205725@gmail.com>
2023-03-12 20:49:45 +0800

    init website

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+/*
+	Rocket Internet by TEMPLATE STOCK
+    templatestock.co @templatestock
+*/
+
+/* Header Full Screen */
+$(document).ready(function() {
+	$('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
+	$(window).resize(function(){
+        $('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
+    });
+
+});
+
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+          $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+
+    else {
+          $('nav').removeClass('black');
+    }
+})
